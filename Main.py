2e491ef08e4ebffcbf13df2691ef4a08f98edfb9
avvinaav1 <kavinav75@gmail.com>
2026-09-12 16:24:27 +0530

    section2.3: add savings and current interest policies

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from AccountRepository import AccountRepository
 from BankAccount import BankAccount
 from NotificationService import NotificationService
+from SavingsInterestPolicy import SavingsInterestPolicy
 from StatementGenerator import StatementGenerator
 
 
@@ -11,6 +12,7 @@ def main():
     account_repository = AccountRepository()
     notification_service = NotificationService()
     statement_generator = StatementGenerator()
+    interest_policy = SavingsInterestPolicy()
 
     if account.deposit(1000):
         notification_service.send(
@@ -27,6 +29,10 @@ def main():
         account_repository.save(account)
 
     print(statement_generator.generate(account))
+    print(
+        "Interest earned: Rs. "
+        + str(interest_policy.calculate(account.get_balance()))
+    )
 
 
 if __name__ == "__main__":
